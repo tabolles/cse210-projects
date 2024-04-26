be975458ed5627938ac137dfb5ec40cf28b15730
Trevor Bolles <tabolles@outlook.com>
2024-04-26 10:05:55 -0600

    Completed Prep 2 exercise

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,86 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your current grade percentage?: ");
+        string TAB_userInput = Console.ReadLine();
+        int TAB_userNum = int.Parse(TAB_userInput);
+        string TAB_letter;
+        int TAB_lastDigit = TAB_userNum % 10;
+
+        if (TAB_userNum >= 90)
+        { 
+            if (TAB_lastDigit < 3)
+            {
+                TAB_letter = "A-";
+            }
+            else
+            {
+                TAB_letter = "A";
+            }
+        }
+        else if (TAB_userNum >= 80)
+        {
+            if (TAB_lastDigit >= 7)
+            {
+                TAB_letter = "B+";
+            }
+            else if (TAB_lastDigit < 3)
+            {
+                TAB_letter = "B-";
+            }
+            else
+            {
+                TAB_letter = "B";
+            }
+        }
+        else if (TAB_userNum >= 70)
+        {
+            if (TAB_lastDigit >= 7)
+            {
+                TAB_letter = "C+";
+            }
+            else if (TAB_lastDigit < 3)
+            {
+                TAB_letter = "C-";
+            }
+            else
+            {
+                TAB_letter = "C";
+            }
+        }
+        else if (TAB_userNum >= 60)
+        {
+            if (TAB_lastDigit >= 7)
+            {
+                TAB_letter = "D+";
+            }
+            else if (TAB_lastDigit < 3)
+            {
+                TAB_letter = "D-";
+            }
+            else
+            {
+                TAB_letter = "D";
+            }
+        }
+        else
+        {
+            TAB_letter = "F";
+        }
+
+        Console.WriteLine($"Grade: {TAB_letter}");
+
+        if (TAB_userNum >= 70)
+        {
+            Console.WriteLine("You passed the class!");
+        } else {
+            Console.WriteLine("You failed the class.");
+        }
+
     }
 }
