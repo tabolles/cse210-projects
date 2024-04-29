d44d312585418f154f8ee638ce19f22a073cf7af
Trevor Bolles <amulet4@byui.edu>
2024-04-29 12:44:59 -0600

    Notes taken for class on 4/29/24 contained in Adolin with a modefied Sandbox for a coding example.

--- sandbox/Adolin/Program.cs
+++ sandbox/Adolin/Program.cs
@@ -7,3 +7,34 @@ class Program
         Console.WriteLine("Hello, World!");
     }
 }
+    /*
+    Class 3 notes:
+    Python vs C#            int x = 0;
+    if  x==0:               if (x==0){ 
+        print("tmit")           Console.WriteLine("tmit");
+                            } 
+    loops
+    while x <3:             while (x<3){
+        print("tmit")           Console.WriteLine("tmit");
+        x+=1                    x+=1; or x++;
+        x-=1                    x-=1; or x--;
+        x*=1                    x*=1; or x=x*1;
+        x/=1                    x/=1; or x=x/1;
+        x%=1                    x%=1; or x=x%1;
+                            }
+                            or
+                            do{
+                                Console.WriteLine("tmit");
+                                x+=1;
+                            }while (x<3);
+
+                                initialize; test; increment
+    for x in range(3):      for (int x=0; x<3; x++){
+        print("tmit")           Console.WriteLine("tmit");
+                            } (or)
+                            foreach (int x in new int[]{0,1,2}){
+                                Console.WriteLine("tmit");
+                            }
+    
+                            
+    */
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // for ( ; ; ) will cause an infinite loop because there is no increment (Ctrl + C to stop)
+        for (int x = 0; x < 9; x++)
+        {
+            Console.WriteLine("The Most Interesting Thing.");
+        }
     }
 }
