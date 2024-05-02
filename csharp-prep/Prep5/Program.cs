19e35fc27882766d7ab66d8b10ff0b602e40ad8a
Trevor Bolles <amulet4@byui.edu>
2024-05-02 15:07:21 -0600

    Revert "Completed Prep 5 Assignment"
    This reverts commit 6691055843b891f7156db0d9e7d228d84dee95d5.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,30 +2,6 @@ using System;
 
 class Program
 {
-    static void DisplayWelcome()
-    {
-        Console.WriteLine("Welcome to the Program!");
-    }
-    static string PromptUserName()
-    {
-        Console.Write("Enter your name: ");
-        string name = Console.ReadLine();
-        return name;
-    }
-    static int PromptUserNumber()
-    {
-        Console.Write("Enter your favorite number: ");
-        int number = int.Parse(Console.ReadLine());
-        return number;
-    }
-    static int SquareNumber(int number)
-    {
-        return number * number;
-    }
-    static void DisplayResult(string name, int number, int squaredNumber)
-    {
-        Console.WriteLine($"{name}, the square of your number is {squaredNumber}.");
-    }
     static void Main(string[] args)
     {
         DisplayWelcome();
@@ -33,8 +9,6 @@ class Program
         int number = PromptUserNumber();
         int squaredNumber = SquareNumber(number);
         DisplayResult(name, number, squaredNumber);
-
-        int GAY = 3;
 
     }
 }
