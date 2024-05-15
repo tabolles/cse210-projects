d4d5a09be4df911b4c15eeeb1f9c67070b620fb3
Trevor Bolles <amulet4@byui.edu>
2024-05-15 11:36:46 -0600

    tweaked some things in Develop02 project

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -13,5 +13,13 @@ class Journal{
         return _entry;
     }
 
+    public string displayToday(){
+        string[] entry = _entry.Split(", ");
+        string date = entry[0];
+        string prompt = entry[1];
+        string response = entry[2];
+        Console.WriteLine($"Date: {date}\nPrompt: {prompt}\nResponse: {response}")
+    }
+
 
 }
