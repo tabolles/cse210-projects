1efec99c33b800fbd1619b88a7ce62e5ff29cb84
Trevor Bolles <amulet4@byui.edu>
2024-05-22 15:53:43 -0600

    Completed the Learning03 challenge.

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,48 @@
+using System;
+
+public class Fraction{
+    private int _top;
+    private int _bottom;
+
+    public Fraction(){
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber){
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom){
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int getTop(){
+        return _top;
+    }
+
+    public int setTop(int top){
+        _top = top;
+        return _top;
+    }   
+
+    public int getBottom(){
+        return _bottom;
+    }
+
+    public int setBottom(int bottom){
+        _bottom = bottom;
+        return _bottom;
+    }
+
+    public string getFractionString(){
+        return _top + "/" + _bottom;
+    }
+
+    public double getDecimalValue(){
+        return (double)_top / _bottom;
+    }
+
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction = new Fraction();
+        Console.WriteLine(fraction.getFractionString());
+        Console.WriteLine(fraction.getDecimalValue());
+
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine(fraction2.getFractionString());
+        Console.WriteLine(fraction2.getDecimalValue());
+
+        Fraction fraction3 = new Fraction(3, 4);
+        Console.WriteLine(fraction3.getFractionString());
+        Console.WriteLine(fraction3.getDecimalValue());
+
+        Fraction fraction4 = new Fraction(1, 3);
+        Console.WriteLine(fraction4.getFractionString());
+        Console.WriteLine(fraction4.getDecimalValue());
     }
 }
